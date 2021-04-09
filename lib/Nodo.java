ec0ca6c9756a81d64d0b4f82f4b672fe351b09fc
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-04-09 12:43:19 -0300

    Atributos tornados PRIVATE, incluido atributo HIGH e getters & setters

--- lib/Nodo.java
+++ lib/Nodo.java
@@ -1,37 +1,84 @@
 
 public class Nodo {
 
-    int data;
-    Nodo parent;
-    Nodo rightSon;
-    Nodo leftSon;
-    int balanceFactor;
+    private int data;
+    private Nodo parent;
+    private Nodo rightSon;
+    private Nodo leftSon;
+    private int hight;
+    private int balanceFactor;
 
     public Nodo (int data) {
         this.data = data;
         this.parent = null;
         this.rightSon = null;
         this.leftSon = null;
+        this.hight = 0;
         this.balanceFactor = 0;
-
     }
 
     public Nodo() {            //construtor vazio para montar uma árvore vazia; 
-
     }
+
+    public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Nodo getParent() {
+		return parent;
+	}
+
+	public void setParent(Nodo parent) {
+		this.parent = parent;
+	}
+
+	public Nodo getRightSon() {
+		return rightSon;
+	}
+
+	public void setRightSon(Nodo rightSon) {
+		this.rightSon = rightSon;
+	}
+
+	public Nodo getLeftSon() {
+		return leftSon;
+	}
+
+	public void setLeftSon(Nodo leftSon) {
+		this.leftSon = leftSon;
+	}
+
+	public int getHight() {
+		return hight;
+	}
+
+	public void setHight(int hight) {
+		this.hight = hight;
+	}
+
+	public int getBalanceFactor() {
+		return balanceFactor;
+	}
+
+	public void setBalanceFactor(int balanceFactor) {
+		this.balanceFactor = balanceFactor;
+	}
+
+    
 
     @Override
     public String toString() {
         return  "Nodo data: " + data +"\n" +
                 "Pai: " + parent + "\n" +
                 "Filho à direita: " + rightSon + "\n" + 
-                "Filho à esquerda: " + leftSon + "\n";
-    
-    }
+                "Filho à esquerda: " + leftSon + "\n" +
+                "Altura do nodo: " + hight + "\n" +
+                "Fator de balanceamento: " + balanceFactor + "\n";
 
-    //nodo nodoPai
-    //nodo nodoFilhoDireita
-    //nodo nodoFilhoEsquerda
-    //int fatorBalanceamento
     
+    }    
 }
