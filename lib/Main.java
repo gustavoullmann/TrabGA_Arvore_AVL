ca42bc1e4cd29bcdb80f1d2468ee16702283590c
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-04-18 21:07:13 -0300

    modificações temporárias apenas para fins de teste

--- lib/Main.java
+++ lib/Main.java
@@ -4,53 +4,67 @@ public class Main {
     static Tree AVL_TREE = new Tree();
     public static void main(String[] args) {
 
+        AVL_TREE.insertNode(12);
+        AVL_TREE.insertNode(6);
+        AVL_TREE.insertNode(18);
+        AVL_TREE.insertNode(2);
+        AVL_TREE.insertNode(9);
+        AVL_TREE.insertNode(15);
+        AVL_TREE.insertNode(20);
+
+        Menu.menu();
+
+
         //Menu.menu();
 
         //TODAS AS LINHAS ABAIXO SÃO APENAS PARA TESTE "BRUTO" DA LÓGICA IMPLEMENTADA ATÉ O MOMENTO. Aparentemente tudo funciona como deveria
         //Descomentar a linha '7' e comentar todas as linhas entre '12' e '54' para testar o programa pelo menu de usuário
 
-        AVL_TREE.insertNode(10);
+        // AVL_TREE.insertNode(10);
         
-        System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
-        System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
-        System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
-        System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());
+        // System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
+        // System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
+        // System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());
 
-        System.out.println("=====================================================================");
+        // System.out.println("=====================================================================");
 
-        AVL_TREE.insertNode(5);
+        // AVL_TREE.insertNode(5);
 
-        System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
-        System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
-        System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
-        System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());
+        // System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
+        // System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
+        // System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());
 
-        System.out.println("=====================================================================");
+        // System.out.println("=====================================================================");
 
-        AVL_TREE.insertNode(15);
+        // AVL_TREE.insertNode(15);
 
-        System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
-        System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
-        System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
-        System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());
+        // System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
+        // System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
+        // System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());
 
-        System.out.println("=====================================================================");
+        // System.out.println("=====================================================================");
 
-        AVL_TREE.insertNode(1);
+        // AVL_TREE.insertNode(1);
 
-        System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
-        System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
-        System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
-        System.out.println("LEFTneto:................." + Main.AVL_TREE.getRoot().getLeftSon().getLeftSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getLeftSon().getLeftSon().printNodeAttributes());
-        System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
-        System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());  
+        // System.out.println("ROOT:...................." + Main.AVL_TREE.getRoot().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().printNodeAttributes());
+        // System.out.println("LEFTSON:................." + Main.AVL_TREE.getRoot().getLeftSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getLeftSon().printNodeAttributes());
+        // System.out.println("LEFTneto:................." + Main.AVL_TREE.getRoot().getLeftSon().getLeftSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getLeftSon().getLeftSon().printNodeAttributes());
+        // System.out.println("RIGHTSON:................." + Main.AVL_TREE.getRoot().getRightSon().toString());
+        // System.out.println(Main.AVL_TREE.getRoot().getRightSon().printNodeAttributes());  
+
+
+        
     }
 }
