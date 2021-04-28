504c622051ae7c11ae1f58ddb6c6a09ab940735b
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-04-27 22:46:16 -0300

    eliminação de TODO e testes

--- lib/Main.java
+++ lib/Main.java
@@ -15,13 +15,13 @@ public class Main {
         //Menu.menu();
 
         AVL_TREE.insertNode(1);                                     //TODO: testar a sequencia 2,3,1 -> deu erro de nullpointer (acredito que esse erro seja por tentar balancear uma árvore balanceada)
-        AVL_TREE.insertNode(3);                                     
-        AVL_TREE.insertNode(2);
+        AVL_TREE.insertNode(2);                                     
+        AVL_TREE.insertNode(3);
         
         AVL_TREE.printTree(AVL_TREE.getRoot(), 0);
 
-        AVL_TREE.rebalanceNode(AVL_TREE.checkTreeUnbalance_FROM_NEWNODE(AVL_TREE.searchNode(1))); //TODO: descobrir porque não roda com searchNode do nó folha 
-
+        AVL_TREE.rebalanceNode(AVL_TREE.checkTreeUnbalance_FROM_NEWNODE(AVL_TREE.searchNode(3))); 
+        
         AVL_TREE.printTree(AVL_TREE.getRoot(), 0);
 
         //AVL_TREE.rightRotation(AVL_TREE.getRoot());
