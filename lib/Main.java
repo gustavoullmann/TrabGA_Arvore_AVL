8a87ad9fefba93361c29c9e3c31bbe2f3b47f189
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-04-29 03:01:45 -0300

    modificações para fins de testes

--- lib/Main.java
+++ lib/Main.java
@@ -4,13 +4,13 @@ public class Main {
     static Tree AVL_TREE = new Tree();                              //Cria uma nova Tree em cada execução do código
     public static void main(String[] args) {
 
-        AVL_TREE.insertNode(12);
-        AVL_TREE.insertNode(6);
-        AVL_TREE.insertNode(18);
-        AVL_TREE.insertNode(2);
-        AVL_TREE.insertNode(9);
-        AVL_TREE.insertNode(15);
-        AVL_TREE.insertNode(20);
+        // AVL_TREE.insertNode(12);
+        // AVL_TREE.insertNode(6);
+        // AVL_TREE.insertNode(18);
+        // AVL_TREE.insertNode(2);
+        // AVL_TREE.insertNode(9);
+        // AVL_TREE.insertNode(15);
+        // AVL_TREE.insertNode(20);
 
         // AVL_TREE.preOrderTraversal(AVL_TREE.getRoot());
         // System.out.println();
@@ -18,13 +18,21 @@ public class Main {
         // System.out.println();
         // AVL_TREE.postOrderTraversal(AVL_TREE.getRoot());
 
-        Menu.menu();
 
-        // AVL_TREE.insertNode(1);                                     //TODO: testar a sequencia 2,3,1 -> deu erro de nullpointer (acredito que esse erro seja por tentar balancear uma árvore balanceada)
-        // AVL_TREE.insertNode(2);                                     
-        // AVL_TREE.insertNode(3);
-        
-        // AVL_TREE.printTree(AVL_TREE.getRoot(), 0);
+        AVL_TREE.insertNode(1);                                     //TODO: testar a sequencia 2,3,1 -> deu erro de nullpointer (acredito que esse erro seja por tentar balancear uma árvore balanceada)
+        AVL_TREE.insertNode(2);                                     
+        AVL_TREE.insertNode(3);
+
+        AVL_TREE.printTree(AVL_TREE.getRoot(), 0);
+
+        AVL_TREE.checkTreeUnbalance_FROM_NEWNODE2(AVL_TREE.searchNode(3));
+
+        AVL_TREE.printTree(AVL_TREE.getRoot(), 0);
+
+
+        //System.out.println(teste.printNodeAttributes());
+
+        //AVL_TREE.printTree(AVL_TREE.getRoot(), 0);
 
         // AVL_TREE.rebalanceNode(AVL_TREE.checkTreeUnbalance_FROM_NEWNODE(AVL_TREE.searchNode(3))); 
         
