11313e1208b5b263d1338e7cbc07e005f1f79c37
Gustavo Ullmann <gustavoullmann@gmail.com>
2021-04-29 22:08:25 -0300

    modificações para testes

--- lib/Main.java
+++ lib/Main.java
@@ -4,6 +4,14 @@ public class Main {
     static Tree AVL_TREE = new Tree();
     public static void main(String[] args) {
 
+        AVL_TREE.insertNode(12);
+        AVL_TREE.insertNode(6);
+        AVL_TREE.insertNode(18);
+        AVL_TREE.insertNode(2);
+        AVL_TREE.insertNode(9);
+        AVL_TREE.insertNode(15);
+        AVL_TREE.insertNode(20);
+
         Menu.menu();
     }
 }
